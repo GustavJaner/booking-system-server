e73122e4f6c856847f25ba995deb58412efbf2d5
forssellm <melkerforssell@gmail.com>
2019-11-27 10:30:00 +0100

    fixed the many to many relationship in accessgroup as well.

--- graphql/resolvers/accessGroupResolver.js
+++ graphql/resolvers/accessGroupResolver.js
@@ -2,10 +2,9 @@ const Access = require("../../models/accessgroup/accessgroup");
 const AccessGroupRoom = require("../../models/accessgrouproom/accessgrouproom");
 const Room = require("../../models/room/room");
 
-
 const resolver = {
   Query: {
-    accessGroups: () => Access.find({}),
+    accessGroups: () => Access.find({})
   },
   Mutation: {
     addAccessGroup: (parent, access) => {
@@ -19,16 +18,19 @@ const resolver = {
         { upsert: false }
       );
     },
-    removeAccessGroup: (parent, access) => {
-      return Access.remove({ _id: access.id });
-    },
+    removeAccessGroup: async (parent, access) => {
+      await AccessGroupRoom.deleteMany({
+        accessGroupId: access.id
+      });
+      return Access.findByIdAndDelete({ _id: access.id });
+    }
   },
   AccessGroup: {
     async rooms(parent) {
       let list = await AccessGroupRoom.find({ accessGroupId: parent._id });
       return await list.map(item => Room.findById({ _id: item.roomId }));
     }
-  },
+  }
 };
 
 module.exports = resolver;
