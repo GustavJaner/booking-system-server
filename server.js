e73539cb07b3eff4535ea3bec5f5c1c72df66734
forssellm <melkerforssell@gmail.com>
2019-12-02 10:25:30 +0100

    start of auth

--- server.js
+++ server.js
@@ -2,14 +2,30 @@ const express = require("express");
 const { PubSub } = require("apollo-server");
 const http = require("http");
 const { ApolloServer } = require("apollo-server-express");
+const passport = require("passport");
+const { GraphQLLocalStrategy, buildContext } = require("graphql-passport");
+
 const mongoose = require("mongoose");
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
-const credentials = require('./credentials');
+const credentials = require("./credentials");
 
+passport.use(
+  new GraphQLLocalStrategy((email, password, done) => {
+    const users = User.getUsers();
+    const matchingUser = users.find(
+      user => email === user.email && password === user.password
+    );
+    const error = matchingUser ? null : new Error("no matching user");
+    done(error, matchingUser);
+  })
+);
 
 mongoose
-  .connect(credentials.MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+  .connect(credentials.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  })
   .then(() => console.log("DB connected"))
   .catch(err => console.log(err));
 
@@ -18,10 +34,11 @@ const pubsub = new PubSub();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: () => ({ pubsub })
+  context: ({ req, res }) => buildContext({ req, res, pubsub })
 });
 
 const app = express();
+app.use(passport.initialize());
 
 server.applyMiddleware({ app });
 const httpServer = http.createServer(app);
