82524b0827ba439f93e338d55177bb7e9454a47a
forssellm <melkerforssell@gmail.com>
2019-12-03 15:13:33 +0100

    clean up of accessgroup

--- graphql/resolvers/accessGroupResolver.js
+++ graphql/resolvers/accessGroupResolver.js
@@ -4,15 +4,16 @@ const User = require("../../models/user/user");
 const AccessGroupRoom = require("../../models/accessgrouproom/accessgrouproom");
 const AccessGroupUser = require("../../models/accessgroupuser/accessGroupUser");
 
-
 const resolver = {
   Query: {
     accessGroups: async () => await AccessGroup.find({}),
-    accessGroup: async (_parent, args) => await AccessGroup.findById({ _id: args.id }),
+    accessGroup: async (_parent, args) =>
+      await AccessGroup.findById({ _id: args.id })
   },
+
   Mutation: {
     addAccessGroup: async (_, args) => {
-      const newAccessGroup = await (new AccessGroup(args)).save();
+      const newAccessGroup = await new AccessGroup(args).save();
 
       return newAccessGroup;
     },
@@ -23,21 +24,22 @@ const resolver = {
       return await AccessGroup.findByIdAndDelete({ _id: args.id });
     },
     updateAccessGroup: async (_, args) => {
-      return await AccessGroup.findOneAndUpdate(
-        { _id: args.id },
-        { ...args },
-      );
-    },
+      return await AccessGroup.findOneAndUpdate({ _id: args.id }, { ...args });
+    }
   },
   AccessGroup: {
     async rooms(parent) {
       let list = await AccessGroupRoom.find({ accessGroupId: parent._id });
-      return list.map(async (item) => await Room.findById({ _id: item.roomId }));
+
+      return Room.find({
+        _id: { $in: list.map(item => item.roomId) }
+      });
     },
     async users(parent) {
       let list = await AccessGroupUser.find({ accessGroupId: parent._id });
-      return list.map(async (item) => await User.findById({ _id: item.userId }));
-    },
+
+      return User.find({ _id: { $in: list.map(item => item.userId) } });
+    }
   }
 };
 
