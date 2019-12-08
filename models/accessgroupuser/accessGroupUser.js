5e627fca9be56e4a770758a648b482e77d9386d3
GustavJaner <gustav.janer@gmail.com>
2019-12-08 12:39:52 +0100

    Fix wrong folder name

--- models/accessgroupuser/accessGroupUser.js
+++ models/accessgroupuser/accessGroupUser.js
@@ -4,6 +4,7 @@ const AccessGroupUserSchema = new Schema({
   userId: String,
   accessGroupId: String
 });
+
 AccessGroupUserSchema.method("toJSON", function() {
   const { __v, _id, ...object } = this.toObject();
   object.id = _id;
